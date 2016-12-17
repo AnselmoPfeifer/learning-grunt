0819912a14e215be9d23367a9b97fb1c194ccce3
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-12-17 11:44:57 -0300

    add git ignore

--- 02-concatenacao-arquivos/Gruntfile.js
+++ 02-concatenacao-arquivos/Gruntfile.js
@@ -35,6 +35,7 @@ module.exports = function(grunt) {
 	// carregando tasks
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
