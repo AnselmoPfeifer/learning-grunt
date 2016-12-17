5a7d65c208281c18f8ca24b9732c3e4e86e83daf
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-12-17 12:44:45 -0300

    Concluindo modulo 03. Otimização de imagens

--- 03-otimizacao-imagens/Gruntfile.js
+++ 03-otimizacao-imagens/Gruntfile.js
@@ -22,24 +22,36 @@ module.exports = function(grunt) {
 
 		usemin: {
 			html: 'dist/**/*.html'
+		},
+
+		imagemin: {
+			public: {
+				expand: true,
+				cwd: 'dist/images',
+				src: '**/*.{png, jpg,gif}',
+				dest: 'dist/images'
+			}
 		}
 
 	});
 
-	//registrando task
+	// registrando task
 	grunt.registerTask('dist', ['clean', 'copy']);
-	grunt.registerTask('minifica', ['useminPrepare',
-		'concat', 'uglify', 'cssmin', 'usemin']);
 
-	// Task default
+	// task minifica
+	grunt.registerTask('minifica', ['useminPrepare',
+		'concat', 'uglify', 'cssmin', 'usemin', 'imagemin']);
+
+	// task default
 	grunt.registerTask('default', ['dist', 'minifica']);
 
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
-
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-usemin');
+
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 
 }
