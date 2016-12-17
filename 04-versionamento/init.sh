94fa9802ed5c2274bc2898261495f46bb744a17c
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-12-17 12:48:26 -0300

    Concluindo modulo 03. Otimização de imagens

---
+++ 04-versionamento/init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# npm init
+# sudo npm install grunt-cli -g
+npm install grunt --save-dev
+npm install grunt-contrib-copy --save-dev
+npm install grunt-contrib-clean --save-dev
+
+npm install grunt-contrib-concat --save-dev
+npm install grunt-contrib-uglify --save-dev
+npm install grunt-contrib-cssmin --save-dev
+npm install grunt-usemin --save-dev
+npm install grunt-contrib-imagemin --save-dev
